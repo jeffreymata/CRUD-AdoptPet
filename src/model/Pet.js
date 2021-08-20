3278e752017cfc7f1a2b520bf06cc7c24ce827ba
KenethM1 <kenethperez56@gmail.comg>
2021-08-19 20:20:22 -0600

    Pet changed

--- src/model/Pet.js
+++ src/model/Pet.js
@@ -2,8 +2,26 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const Pet = Schema({
-  NombreMascota: String,
-  edad: Number,
+  Organization: {
+    Name: String,
+    Address: {
+      City: String,
+      Street: String,
+      Avenue: String,
+      Lat: String,
+      Lon: String
+    },
+  },
+  PetName: String,
+  Age: Number,
+  Description: String,
+  Images: [{
+    image: String
+  }],
+  meta: {
+    DateCreated: String
+  }
 });
+
 
 module.exports = mongoose.model('Pets', Pet);
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
+const mongoose = require('mongoose')
 const router = express.Router();
-const Pet = require('../model/Pet');
+const { Pet } = require('../model/Pet');
 
 router.get('/', async (req, res) => {
   const pet = await Pet.find();
@@ -10,51 +11,55 @@ router.get('/', async (req, res) => {
 });
 
 router.post('/add', async (req, res, next) => {
-  const { NombreMascota, edad } = req.body;
-  console.log("puto");
-  if (NombreMascota && edad) {
-    const pet = new Pet(req.body);
-    await Pet.collection.insertOne(pet);
-    res.json(pet);
-  }
-  res.json({ "error": "No mandaste ningun dato pendejo" })
+  const { NombreMascota, edad, Organization, meta } = req.body;
 
-});
+  if (NombreMascota && edad && Organization) {
 
-router.get('/turn/:id', async (req, res, next) => {
-  let { id } = req.params;
-  const pet = await Pet.findById(id);
-  pet.status = !pet.status;
-  await pet.save();
-  res.redirect('/');
-});
+    const pet = new Pet({
+      PetName: req.body.PetName,
+      Age: req.body.Age,
+      Description: req.body.Description,
+      Images: req.body.Images,
+      Organization: req.body.Organization,
+      meta: Date.now()
+    });
+
+    try {
+      console.log(mongoose.connection.readyState);
+      Pet.collection.insertOne(pet);
+    } catch (error) {
+      res.send(error);
+    };
+    res.send(pet);
+  };
 
 
-router.get('/edit/:id', async (req, res, next) => {
-  const pet = await Pet.findById(req.params.id);
-  console.log(pet)
-  res.render('edit', { task: pet });
-});
+  router.get('/edit/:id', async (req, res, next) => {
+    const pet = await Pet.findById(req.params.id);
+    console.log(pet)
+    res.render('edit', { task: pet });
+  });
 
-router.post('/edit/:id', async (req, res, next) => {
-  const { id } = req.params;
-  await Pet.update({ _id: id }, req.body);
-  res.redirect('/');
-});
+  router.post('/edit/:id', async (req, res, next) => {
+    const { id } = req.params;
+    await Pet.update({ _id: id }, req.body);
+    res.redirect('/');
+  });
 
-router.get('/delete/:id', async (req, res, next) => {
-  let { id } = req.params;
-  await Pet.remove({ _id: id });
-  res.redirect('/');
-});
+  router.get('/delete/:id', async (req, res, next) => {
+    let { id } = req.params;
+    await Pet.remove({ _id: id });
+    res.redirect('/');
+  });
 
-router.get('/getAllPets', async (req, res, next) => {
-  try {
-    const pets = await Pet.find();
-    res.send(pets);
-  } catch (error) {
-    next(error);
-  }
+  router.get('/getAllPets', async (req, res, next) => {
+    try {
+      const pets = await Pet.find();
+      res.send(pets);
+    } catch (error) {
+      next(error);
+    }
+  });
 });
 
 
